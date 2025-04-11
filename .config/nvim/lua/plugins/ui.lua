e7d18c42840d4d2c812ef3a6a215c7db8eca4a75
girvel <widauka@ya.ru>
2025-04-11 14:11:33 +0500

    nvim: Better TTY compatibility

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -6,6 +6,8 @@ return {
   {
     "loctvl842/monokai-pro.nvim",
     config = function()
+      if vim.env.TERM == "linux" then return end
+
       require("monokai-pro").setup {
         filter = "spectrum",
       }
