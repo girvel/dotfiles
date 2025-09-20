b46c9d7cda9793a7ed84a83521c5bb592c0b507b
girvel <widauka@ya.ru>
2025-09-20 19:39:33 +0500

    nvim: Enable bufferline

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,12 +1,11 @@
-return {}
--- return {
---   "akinsho/bufferline.nvim",
---   dependencies = "nvim-tree/nvim-web-devicons",
---   config = function()
---     require("bufferline").setup {
---       options = {
---         separator_style = "slant",
---       },
---     }
---   end,
--- },
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("bufferline").setup {
+      options = {
+        separator_style = "slant",
+      },
+    }
+  end,
+}
