7fea869f9f97e87cd9d3122a917439ae31064d72
girvel <widauka@ya.ru>
2025-10-04 21:58:25 +0500

    nvim: Non-destructive <leader>oo

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -27,15 +27,19 @@ return {
 
     Map("n", "<leader>oo", function()
       local files = vim.fn.globpath(".", "**/*.lua", true, true)
+      local counter = 0
       for _, path in ipairs(files) do
         path = vim.fn.fnamemodify(path, ":p")
         local n = vim.fn.bufadd(path)
-        vim.fn.bufload(n)
-        vim.bo[n].buflisted = false
+        if vim.fn.bufloaded(n) == 0 then
+          vim.fn.bufload(n)
+          vim.bo[n].buflisted = false
+          counter = counter + 1
+        end
       end
 
       if #files > 0 then
-        vim.notify(("LuaLS fix: loaded %s files"):format(#files))
+        vim.notify(("LuaLS fix: loaded %s files"):format(counter))
       end
     end, {})
 
