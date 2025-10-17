4b58536051599f4f27bcadd227d3535056008f14
girvel <widauka@ya.ru>
2025-10-17 23:00:18 +0500

    nvim: Fix initial empty buffer logic

--- .config/nvim/lua/plugins/neo_tree.lua
+++ .config/nvim/lua/plugins/neo_tree.lua
@@ -41,7 +41,15 @@ return {
       pattern = '*',
       callback = function()
         vim.schedule(function()
-          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+          local buffers = vim.tbl_filter(function(buf)
+            return vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted
+          end, vim.api.nvim_list_bufs())
+
+          if #buffers <= 2 then return end
+
+          vim.notify(vim.inspect(buffers))
+
+          for _, buf in ipairs(buffers) do
             if vim.bo[buf].filetype == "" and vim.api.nvim_buf_get_name(buf) == "" then
               vim.api.nvim_buf_delete(buf, {force = true})
               break
