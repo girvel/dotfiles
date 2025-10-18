3cde1d9a99aa9e7087fc86607bcf3d0e37aece0a
girvel <widauka@ya.ru>
2025-10-18 21:18:13 +0500

    nvim: History navigation from insert & visual modes

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -12,6 +12,11 @@ keymap.init = function()
   Api.rumap("n", "<leader>lf", vim.diagnostic.open_float, {})
   Api.rumap("n", "gD", vim.lsp.buf.declaration, {})
 
+  Api.rumap("i", "<C-o>", "<Esc><C-o>", {})
+  Api.rumap("i", "<C-i>", "<Esc><C-i>", {})
+  Api.rumap("v", "<C-o>", "<Esc><C-o>", {})
+  Api.rumap("v", "<C-i>", "<Esc><C-i>", {})
+
   -- shift safety --
   Api.rumap("n", ":W", ":w")
 end
