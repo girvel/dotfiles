3f8c742f189be6bd7ae59e92b292386a2966c577
girvel <widauka@ya.ru>
2025-10-18 14:07:06 +0500

    nvim: Feed(sequence) helper function

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,13 @@
+-- TODO Api global object?
 Map = require("rumap")
+
+--- @param sequence string
+Feed = function(sequence)
+  vim.api.nvim_feedkeys(
+    vim.api.nvim_replace_termcodes(sequence, true, false, true),
+    "n", false
+  )
+end
 
 require("opt").init()
 require("package_manager").init()
