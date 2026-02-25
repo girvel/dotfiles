7809c219a4b2bcdd088d64be4ff9b47daf775bf9
girvel <widauka@ya.ru>
2026-02-25 17:10:37 +0500

    nvim: add oil (disabled for now)

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+  -- TODO consider switching to oil fully
+
+  -- "stevearc/oil.nvim",
+  -- --- @module 'oil'
+  -- --- @type oil.SetupOpts
+  -- opts = {},
+  -- dependencies = {{"nvim-mini/mini.icons", opts = {}}},
+  -- lazy = false,
+}
