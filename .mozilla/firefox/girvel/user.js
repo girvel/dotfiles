82a6abf5bf872a2ad681c1616b1ac10a608d4f1d
girvel <widauka@ya.ru>
2026-03-02 10:45:45 +0500

    firefox: Vertical tabs

--- .mozilla/firefox/girvel/user.js
+++ .mozilla/firefox/girvel/user.js
@@ -1,3 +1,4 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("devtools.chrome.enabled", true);
 user_pref("devtools.debugger.remote-enabled", true);
+user_pref("sidebar.verticalTabs", true);
