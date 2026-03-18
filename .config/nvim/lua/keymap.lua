a2d61f85a25ed31829d01634905440c71dc417ba
girvel <widauka@ya.ru>
2026-03-18 10:49:08 +0500

    nvim: Descriptions in keymap.lua

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -1,21 +1,22 @@
 local keymap = {}
+
 keymap.init = function()
   Api.rumap("i", "<M-o>", function()
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>ggO", true, false, true), "n", false)
-  end, {remap = true})
+  end, {desc = "Newline at the beginning of the file", remap = true})
 
   Api.rumap("n", "<M-o>", function()
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("ggO", true, false, true), "n", false)
-  end, {remap = true})
+  end, {desc = "Newline at the beginning of the file", remap = true})
 
-  Api.rumap("n", "<leader>lr", vim.lsp.buf.rename, {})
-  Api.rumap("n", "<leader>lf", vim.diagnostic.open_float, {})
+  Api.rumap("n", "<leader>lr", vim.lsp.buf.rename, {desc = "LSP rename variable"})
+  Api.rumap("n", "<leader>lf", vim.diagnostic.open_float, {desc = "LSP see warning"})
   Api.rumap("n", "gD", vim.lsp.buf.declaration, {})
 
-  Api.rumap("n", "<leader>dd", "^t]rx")
-  Api.rumap("n", "<leader>ds", "^t]r ")
+  Api.rumap("n", "<leader>dd", "^t]rx", {desc = "Complete markdown task"})
+  Api.rumap("n", "<leader>ds", "^t]r ", {desc = "Uncomplete markdown task"})
 
-  Api.rumap("i", "<D-Space>", "")  -- fixes GNOME issues
+  Api.rumap("i", "<D-Space>", "", {desc = "Empty action to fix GNOME issues"})
 end
 
 return keymap
