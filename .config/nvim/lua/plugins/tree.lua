70b444c5b734a577ff5ef63b6e0086aad6d942b2
girvel <widauka@ya.ru>
2026-03-20 14:39:12 +0500

    nvim: Closing nvim-tree

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -4,5 +4,6 @@ return {
     require("nvim-tree").setup({})
     Api.rumap("n", "<leader>tf", ":NvimTreeFocus<CR>")
     Api.rumap("n", "<leader>tr", ":NvimTreeRefresh<CR>")
+    Api.rumap("n", "<leader>tc", ":NvimTreeClose<CR>")
   end
 }
