f9593ebfb034db2c502efdd6613a249a1bf8e48f
girvel <widauka@ya.ru>
2026-04-03 14:29:39 +0500

    nvim: Fix :wqa with flatten

--- .config/nvim/lua/plugins/flatten.lua
+++ .config/nvim/lua/plugins/flatten.lua
@@ -1,5 +1,8 @@
 return {
   "willothy/flatten.nvim",
-  config = true,
+  config = function()
+    require("flatten").setup {}
+    Api.rumap("ca", "wqa", "wa | qa", {desc = "Write all files & quit; overloaded for flatten compatibility"})
+  end,
   lazy = false,
 }
