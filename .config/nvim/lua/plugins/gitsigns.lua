98983a9ac93830b16e3848cdc392d57906fd47eb
girvel <widauka@ya.ru>
2026-04-21 10:45:41 +0500

    nvim: Fix gitsigns

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,18 @@
 return {
   "lewis6991/gitsigns.nvim",
   opts = {},
+  config = function(_, opts)
+    local gitsigns = require("gitsigns")
+    gitsigns.setup(opts)
+
+    -- gitsigns' refresh does not work by default
+    vim.api.nvim_create_autocmd({"FocusGained", "BufEnter"}, {
+      pattern = "*",
+      callback = function()
+        vim.schedule(function()
+          gitsigns.refresh()
+        end)
+      end,
+    })
+  end,
 }
