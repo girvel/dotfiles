3bd416c8415703d7f29b3d06571729b76cb89199
girvel <widauka@ya.ru>
2026-05-16 20:54:51 +0500

    nvim: Opening files without keeping nvim-tree

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -2,7 +2,21 @@ return {
   "nvim-tree/nvim-tree.lua",
   custom_tags = {"lite"},
   config = function()
-    require("nvim-tree").setup({})
+    require("nvim-tree").setup({
+      on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+        local open_inplace = function()
+          api.node.open.edit()
+          api.tree.close()
+        end
+
+        api.map.on_attach.default(bufnr)
+        Api.rumap("n", "o", open_inplace, {buffer = bufnr})
+        if Config.is_typewriter then
+          Api.rumap("n", "<CR>", open_inplace, {buffer = bufnr})
+        end
+      end,
+    })
     Api.rumap("n", "<leader>tf", ":NvimTreeFocus<CR>")
     Api.rumap("n", "<leader>tr", ":NvimTreeRefresh<CR>")
     Api.rumap("n", "<leader>tc", ":NvimTreeClose<CR>")
