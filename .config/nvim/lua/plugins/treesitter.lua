030a7949611558e84f6ced73a4439aeb20923b9a
girvel <widauka@ya.ru>
2026-06-17 11:54:40 +0500

    nvim: switch treesitter to main branch

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,36 +2,32 @@ return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
   build = ":TSUpdate",
-  branch = "master",
+  branch = "main",
   config = function()
     -- NixOS gets weird without that
     local parser_path = vim.fn.stdpath("data") .. "/treesitter-parsers"
     vim.opt.runtimepath:prepend(parser_path)
 
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "bash",
-        "c",
-        "go",
-        "glsl",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "python",
-        "rust",
-        "toml",
-        "toml",
-        "typescript",
-        "xml",
-        "yaml",
-      },
-
-      highlight = {
-        enable = true,
-      },
-    })
+    local treesitter = require("nvim-treesitter")
+    treesitter.setup()
+    treesitter.install {
+      "bash",
+      "c",
+      "go",
+      "glsl",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "python",
+      "rust",
+      "toml",
+      "toml",
+      "typescript",
+      "xml",
+      "yaml",
+    }
 
     vim.wo.foldmethod = "expr"
     vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
